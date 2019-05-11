e04a859362a1757f72057e1d673d0868429b53b4
Hyeongchan ham <hyoungchan@unist.ac.kr>
2019-05-11 12:15:04 +0900

    q1

---
+++ q1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+	vector<int> v;
+	
+	int input;
+	while(cin>>input){
+		v.push_back(input);
+	}
+
+	int sum = 0;
+	int st_idx=0;
+	int end_idx=0;
+	for(vector<int>::size_type i = 0; i<v.size(); i++){
+		sum += vector[i];
+
+
+}
